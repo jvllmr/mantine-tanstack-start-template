f320008e2342c0f91f69df716a243cbf5d0ae8dc
Jan Vollmer <jan@vllmr.dev>
2025-12-19 19:22:05 +0100

    add build instructions

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # mantine-tanstack-start-template
 
 Minimal TanStack Start + Mantine + pnpm template
+
+## Development
+
+### Install dependencies
+
+```shell
+pnpm install --frozen-lock
+```
+
+### Start dev server
+
+```shell
+# Access via http://localhost:3000
+pnpm dev
+```
+
+## Build for distribution
+
+### Build
+
+```shell
+pnpm build
+```
+
+### Start built server
+
+```shell
+node .output/server/index.mjs
+```
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
-
+import { nitro } from 'nitro/vite';
 export default defineConfig({
   server: {
     port: 3000,
@@ -13,5 +13,6 @@ export default defineConfig({
     tanstackStart(),
     // react's vite plugin must come after start's vite plugin
     viteReact(),
+    nitro(),
   ],
 });
